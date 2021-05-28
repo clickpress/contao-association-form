eedf23b333dc6fe834136d93235185dcff5cf981
Stefan Schulz-Lauterbach <1564251+stefansl@users.noreply.github.com>
2021-05-28 11:42:42 +0200

    Delete src/Resources/contao/config directory

--- src/Resources/contao/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-/*
- * This file is part of the ContaoAssociationFormBundle for Contao.
- *
- * (c) Stefan Schulz-Lauterbach
- *
- * @license LGPL-3.0-or-later
- */
