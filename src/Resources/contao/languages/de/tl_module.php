7b06739e3b9fcbd603cbe56462b7e7266d13557d
stefansl <ssl@clickpress.de>
2022-08-04 15:26:36 +0200

    Fix wording

--- src/Resources/contao/languages/de/tl_module.php
+++ src/Resources/contao/languages/de/tl_module.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_module']['add_notification'] = ['Mail an Admin', 'Soll auch eine Bestätigungsseite an den Admin gesendet werden?'];
+$GLOBALS['TL_LANG']['tl_module']['add_notification'] = ['Mail an Admin', 'Soll auch eine Bestätigung an den Admin gesendet werden?'];
 $GLOBALS['TL_LANG']['tl_module']['notification_mail'] = ['E-Mail-Adresse', 'An welche Adresse soll die Bestätigung geschickt werden?'];
 $GLOBALS['TL_LANG']['tl_module']['privacy_url'] = ['Datenschutz', 'Zu welcher Datenschutz-Seite soll im Formular verlinkt werden?'];
 $GLOBALS['TL_LANG']['tl_module']['statute_url'] = ['Vereinssatzung', 'Auf welcher Seite ist die Vereinssatzung zu finden?'];
