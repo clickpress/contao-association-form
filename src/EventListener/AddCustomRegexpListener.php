efeca900430ab890b156509cabe6e471da3499e2
stefansl <ssl@clickpress.de>
2025-10-06 20:03:19 +0200

    Remove header comments from `AddCustomRegexpListener`

--- src/EventListener/AddCustomRegexpListener.php
+++ src/EventListener/AddCustomRegexpListener.php
@@ -1,15 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of the ContaoAssociationFormBundle for Contao.
- *
- * (c) Stefan Schulz-Lauterbach
- *
- * @license LGPL-3.0-or-later
- */
-
 namespace Clickpress\ContaoAssociationFormBundle\EventListener;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
